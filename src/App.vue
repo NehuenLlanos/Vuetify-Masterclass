965f5b299c16c334e317ecad873a3658a680fd50
NehuenLlanos <nehuengllanos@gmail.com>
2022-09-24 23:36:09 -0300

    up to class 5

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,18 @@
 <template>
+  <!--
+  Route component que utlizamos en el vuetify
+  Todos los componentes que usemos de vuetify tienen que estar dentro de aca.
+  Seria como un mounting point para todos los componentes
+  -->
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+<!--
+    Cada componente de vuetify renderiza su propio html que nosotros no sabemos como es
+    porque no nos interesa pero sabemos que hace algo yendo a la página con todos los componentes
+    -->
+<!--
+    Podemos colocarle un monton de propiedades a la app-bar que tenemos que ir viendo en la cartilla
+    de vuetify,
+    -->
 
     <v-main>
       <router-view/>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,49 @@
 <template>
-  <hello-world />
+  <div class="home">
+
+<!--    <h1>Homepage</h1>-->
+
+<!--    <v-btn class="pink white&#45;&#45;text">Click me</v-btn>-->
+<!--    &lt;!&ndash; El depressed me saca la sombra   &ndash;&gt;-->
+<!--    <v-btn depressed color="pink">Click me 2</v-btn>-->
+<!--    &lt;!&ndash; El flat hace que el background sea transparente, pero cuando nos paramos arriba se pone del color seteado &ndash;&gt;-->
+<!--    <v-btn flat color="pink">Click me 3</v-btn>-->
+<!--&lt;!&ndash;    Podemos agregarle un icono &ndash;&gt;-->
+<!--    <v-btn depressed class="pink white&#45;&#45;text">-->
+<!--      &lt;!&ndash; Con el left separamos un poquito &ndash;&gt;-->
+<!--      <v-icon left>mdi-message-text</v-icon>-->
+<!--      <span>Message</span>-->
+<!--    </v-btn>-->
+<!--    &lt;!&ndash; Vamos a crear un boton mas pequeno   &ndash;&gt;-->
+<!--    <v-btn depressed small class="pink white&#45;&#45;text">-->
+<!--      &lt;!&ndash; Con el left separamos un poquito y con el small lo hacemos pequenio &ndash;&gt;-->
+<!--      <v-icon left small>mdi-message-text</v-icon>-->
+<!--      <span>Message</span>-->
+<!--    </v-btn>-->
+
+<!--    &lt;!&ndash; Vamos a crear un boton mas grande   &ndash;&gt;-->
+<!--    <v-btn depressed large class="pink white&#45;&#45;text">-->
+<!--      <span>Message</span>-->
+<!--      <v-icon right large>mdi-message-text</v-icon>-->
+<!--    </v-btn>-->
+<!--    &lt;!&ndash; Con el fab creamos un botoncito de favourite redondo   &ndash;&gt;-->
+<!--    <v-btn fab depressed small dark color="purple">-->
+<!--      <v-icon >mdi-message-text</v-icon>-->
+<!--    </v-btn>-->
+<!--    <h1>home</h1>-->
+<!--&lt;!&ndash;    Crea un parrafo con fondo rojo y color de texto blanco &ndash;&gt;-->
+<!--    <p class="red white&#45;&#45;text">Lorem ipsum dolor sit</p>-->
+<!--    <p class="pink lighten-4 red&#45;&#45;text text&#45;&#45;darken-4">Lorem ubdfbasdfb bkadb fjf</p>-->
+<!--    <h1 class="display-4">Massive Display</h1>-->
+<!--    <h4 class="display-1">Smaller Display</h4>-->
+<!--    <p class="headline">This is a headline</p>-->
+<!--    <p class="subheading font-weigth-bold">This is a subheading</p>-->
+<!--    <p class="caption text-uppercase" >This is a caption</p>-->
+<!--  </div>-->
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+    name: 'HomeView',
   }
 </script>
